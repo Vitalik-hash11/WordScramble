a1d4766f1f9a4e4f56549879a9e00e76273d5b6e
Vitalii Karpiv <vitalii.karpiv@plus4u.net>
2022-07-04 21:47:54 +0300

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by newbie on 04.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
